f08f27a8bd0770d11770c101e0b651287bb2a61f
SoYoon Ahn <bluingpetal28@gmail.com>
2022-12-29 23:59:06 +0900

    feat : add camera zoom in/out, add max value

--- Assets/Scripts/Camera/CameraController.cs
+++ Assets/Scripts/Camera/CameraController.cs
@@ -8,9 +8,20 @@ public class CameraController : MonoBehaviour
     // 터치하고 왼쪽으로 하면 화면 오른쪽으로 이동하기
     // 최대 최소 설정하기
     private bool controllable;
+    private float totalY = 0;
 
     [SerializeField]
     private float moveSensitivity;
+    [SerializeField]
+    private float zoomSpeed;
+    [SerializeField]
+    private Vector2 maxPos;
+    [SerializeField]
+    private Vector2 minPos;
+    [SerializeField]
+    private float maxZoom;
+    [SerializeField]
+    private float minZoom;
 
     private void Start()
     {
@@ -34,12 +45,26 @@ public class CameraController : MonoBehaviour
 
     private void Move()
     {
-        if(controllable)
+        if (controllable)
         {
-            if(Input.GetMouseButton(0))
+            // Input은 추후 new input system 고려
+            // float deltaY = Input.GetAxis("Zoom") * zoomSpeed * Time.deltaTime;
+            float deltaY = -Input.mouseScrollDelta.y * zoomSpeed * Time.deltaTime;
+            // 최대, 최소 줌 구현
+            if (((transform.localPosition.y + deltaY) < maxZoom) && ((transform.localPosition.y + deltaY) > minZoom))
             {
-                // TODO : clamp 함수 통해서 최대 최소 범위 생성해주기
-                transform.Translate(-Input.GetAxis("Mouse X") * moveSensitivity * Time.deltaTime, 0, -Input.GetAxis("Mouse Y") * moveSensitivity * Time.deltaTime, Space.World);
+                transform.Translate(0, deltaY, 0, Space.World);
+            }
+
+            if (Input.GetMouseButton(0))
+            {
+                float deltaX = -Input.GetAxis("Mouse X") * moveSensitivity * Time.deltaTime;
+                float deltaZ = -Input.GetAxis("Mouse Y") * moveSensitivity * Time.deltaTime;
+                // 최대, 최소 위치 구현
+                if (((transform.localPosition.x + deltaX) < maxPos.x) && ((transform.localPosition.x + deltaX) > minPos.x))
+                    transform.Translate(deltaX, 0, 0, Space.World);
+                if (((transform.localPosition.z + deltaZ) < maxPos.y) && ((transform.localPosition.z + deltaZ) > minPos.y))
+                    transform.Translate(0, 0, deltaZ, Space.World);
             }
         }
     }

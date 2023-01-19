2f1d84f955148b8e95ee242a8e116b5a43eb2156
SoYoon Ahn <bluingpetal28@gmail.com>
2023-01-19 21:09:40 +0900

    feat : add TitleScene

--- Assets/Scripts/GameObjects/ToGameScene.cs
+++ Assets/Scripts/GameObjects/ToGameScene.cs
@@ -5,11 +5,31 @@ using UnityEngine.SceneManagement;
 
 public class ToGameScene : MonoBehaviour
 {
+    private bool startGame;
+    [SerializeField]
+    private Animator camAnimator;
+    [SerializeField]
+    private Animator UIAnimator;
+
+    private void Start()
+    {
+        startGame = false;
+    }
+
     private void Update()
     {
-        if(Input.anyKeyDown)
+        if(Input.anyKeyDown && !startGame)
         {
-            SceneManager.LoadScene("GameScene");
+            startGame = true;
+            camAnimator.SetBool("StartGame", true);
+            UIAnimator.SetBool("StartGame", true);
+            StartCoroutine(GameSceneWaitTime());
         }
+    }
+
+    private IEnumerator GameSceneWaitTime()
+    {
+        yield return new WaitForSeconds(3f);
+        SceneManager.LoadScene("GameScene");
     }
 }
